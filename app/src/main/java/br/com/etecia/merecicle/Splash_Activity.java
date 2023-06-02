cf35cde04005a362e5df0782774c7b7bfc2d6564
luismiguel172 <luis.goncalves68@etec.sp.gov.br>
2023-06-01 21:42:30 -0300

    3

--- app/src/main/java/br/com/etecia/merecicle/Splash_Activity.java
+++ app/src/main/java/br/com/etecia/merecicle/Splash_Activity.java
@@ -2,7 +2,9 @@ package br.com.etecia.merecicle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_Activity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Splash_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),
+                Login_Activity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
